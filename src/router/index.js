2932ca8f2b9539d3cfd916b141a82ad84abd47e6
calvin <puram.calvin@gmail.com>
2021-02-25 16:42:13 +0100

    fix: change relative path o router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,52 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import routes from "./routes";
+// import routes from "./routes";
+
+import Login from "@/views/Login";
+import ForgotPassword from "@/views/ForgotPassword";
+import Register from "@/views/Register";
+import Donor from "@/views/Donor";
+import Clinic from "@/views/Clinic";
+import Agent from "@/views/Agent";
+import ClinicDashboard from "@/views/Dashboard";
+
+export default [
+  {
+    name: "home",
+    path: "/",
+    component: Login,
+  },
+  {
+    name: "ForgotPassword",
+    path: "/forgot_password",
+    component: ForgotPassword,
+  },
+  {
+    name: "Register",
+    path: "/register",
+    component: Register,
+  },
+  {
+    name: "Donor",
+    path: "/register/donor",
+    component: Donor,
+  },
+  {
+    name: "Clinic",
+    path: "/register/clinic",
+    component: Clinic,
+  },
+  {
+    name: "Agent",
+    path: "/register/agent",
+    component: Agent,
+  },
+  {
+    name: "ClinicDashboard",
+    path: "/clinic/home",
+    component: ClinicDashboard,
+  },
+];
 
 Vue.use(VueRouter);
 export const router = new VueRouter({
