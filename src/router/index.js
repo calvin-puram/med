d8c5a736301a9ef19f648e96d093f748d3be568b
calvin <puram.calvin@gmail.com>
2021-02-25 17:23:30 +0100

    fix: change views and utils naming convention

--- src/router/index.js
+++ src/router/index.js
@@ -1,52 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-// import routes from "./routes";
-
-import Login from "@/Views/Login";
-import ForgotPassword from "@/Views/ForgotPassword";
-import Register from "@/Views/Register";
-import Donor from "@/Views/Donor";
-import Clinic from "@/Views/Clinic";
-import Agent from "@/Views/Agent";
-import ClinicDashboard from "@/Views/Dashboard";
-
-const routes = [
-  {
-    name: "home",
-    path: "/",
-    component: Login,
-  },
-  {
-    name: "ForgotPassword",
-    path: "/forgot_password",
-    component: ForgotPassword,
-  },
-  {
-    name: "Register",
-    path: "/register",
-    component: Register,
-  },
-  {
-    name: "Donor",
-    path: "/register/donor",
-    component: Donor,
-  },
-  {
-    name: "Clinic",
-    path: "/register/clinic",
-    component: Clinic,
-  },
-  {
-    name: "Agent",
-    path: "/register/agent",
-    component: Agent,
-  },
-  {
-    name: "ClinicDashboard",
-    path: "/clinic/home",
-    component: ClinicDashboard,
-  },
-];
+import routes from "./routes";
 
 Vue.use(VueRouter);
 export const router = new VueRouter({
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,10 @@
-import Login from "@/views/Login";
-import ForgotPassword from "@/views/ForgotPassword";
-import Register from "@/views/Register";
-import Donor from "@/views/Donor";
-import Clinic from "@/views/Clinic";
-import Agent from "@/views/Agent";
-import ClinicDashboard from "@/views/Dashboard";
+import Login from "@/Views/Login";
+import ForgotPassword from "@/Views/ForgotPassword";
+import Register from "@/Views/Register";
+import Donor from "@/Views/Donor";
+import Clinic from "@/Views/Clinic";
+import Agent from "@/Views/Agent";
+import ClinicDashboard from "@/Views/Dashboard";
 
 export default [
   {
