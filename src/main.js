4ec24ca5c146471d11c18c68067f0bcdc9e66c4f
calvin <puram.calvin@gmail.com>
2021-02-25 17:48:04 +0100

    chore: add global component

--- src/main.js
+++ src/main.js
@@ -4,7 +4,36 @@ import { router } from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import store from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
-import registerGlobalComponents from "./components/_globals";
+import upperFirst from "lodash/upperFirst";
+import camelCase from "lodash/camelCase";
+
+// automatic component registration
+const requireComponent = require.context(
+  ".",
+
+  true,
+
+  /_base-[A-Z]\w+\.(vue|js)$/
+);
+
+requireComponent.keys().forEach((fileName) => {
+  const componentConfig = requireComponent(fileName);
+
+  const componentName = upperFirst(
+    camelCase(
+      fileName
+        .split("/")
+        .pop()
+        .replace(/\.\w+$/, "")
+    )
+  );
+
+  Vue.component(
+    componentName,
+
+    componentConfig.default || componentConfig
+  );
+});
 
 new Vue({
   router,
